9368d60509d93f561db68b955dd96a7b82b56d2a
out-de-e <EDDe@sberbank.ru>
2021-09-18 16:35:31 +0300

    init commit

---
+++ src/main/java/com/spring/mvc_hibernate_aop/service/EmployeeService.java
@@ -0,0 +1,11 @@
+package com.spring.mvc_hibernate_aop.service;
+
+import com.spring.mvc_hibernate_aop.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    public List<Employee> getAllEmployees();
+
+    public void saveEmployees(Employee employee);
+}
